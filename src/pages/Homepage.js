bcd43be2c0646a95fb78dbadf10814f80c629434
Danny Kao <danny00912@gmail.com>
2022-07-20 19:44:33 +0800

    test 2

---
+++ src/pages/Homepage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import HeaderComponent from "../components/Header-component";
+import NavComponent from "../components/Nav-component";
+import SectionComponent from "../components/Section-component";
+
+const Homepage = () => {
+  return (
+    <div>
+      <HeaderComponent />
+      <NavComponent />
+      <SectionComponent />
+    </div>
+  );
+};
+
+export default Homepage;
---
+++ src/pages/HotPage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import HeaderComponent from "../components/Header-component";
+
+const HotPage = () => {
+  const changeClothe = () => {
+    document.querySelector(
+      "img.li"
+    ).src = require(`../photo-resource/man-clothe/clothe 0/COL03-clothe.jpg`);
+  };
+
+  const changeClothe1 = () => {
+    document.querySelector(
+      "img.li"
+    ).src = require(`../photo-resource/man-clothe/clothe 0/COL00-clothe.jpg`);
+  };
+
+  return (
+    <div className="COL00-clothe">
+      <HeaderComponent />
+      <div className="container">
+        <a href="#">
+          <img
+            className="li"
+            src={require("../photo-resource/man-clothe/clothe 0/COL00-clothe.jpg")}
+            alt="COL00-clothe"
+          />
+        </a>
+
+        <div>
+          <ul>
+            <li>
+              <img
+                onClick={changeClothe1}
+                src={require("../photo-resource/man-clothe/clothe 0/COL00.jpg")}
+                alt=""
+              />
+            </li>
+            <li>
+              <img
+                onClick={changeClothe}
+                src={require("../photo-resource/man-clothe/clothe 0/COL03.jpg")}
+                alt=""
+              />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HotPage;
